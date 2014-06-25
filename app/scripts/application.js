5f2e5f40246aea7faccc97c0278cf874a8a8e89c
Grant Stampfli <mail@gstampfli.com>
2014-06-25 15:45:04 -0700

    added a person model

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -4,3 +4,13 @@
 
 var AddressBook = Ember.Application.create();
 require('./router.js')(AddressBook);
+
+AddressBook.Person = Ember.Object.extend({
+	firstName: null,
+	lastName: null
+});
+var ironman = AddressBook.Person.create({
+	firstName: 'Tian',
+	lastName: 'Song'
+});
+console.log(ironman);
