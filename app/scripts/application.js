3e968a42dde6344722aa69e92e02d6af976ed8b2
Grant Stampfli <mail@gstampfli.com>
2014-06-26 09:49:46 -0700

    set up a controller

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -5,12 +5,34 @@
 var AddressBook = Ember.Application.create();
 require('./router.js')(AddressBook);
 
-AddressBook.Person = Ember.Object.extend({
-	firstName: null,
-	lastName: null
+AddressBook.ApplicationAdapter = DS.FixtureAdapter;
+
+var attr = DS.attr();
+AddressBook.Person = DS.Model.extend({
+	firstName: attr('string'),
+	lastName: attr('string')
 });
-var ironman = AddressBook.Person.create({
-	firstName: 'Tian',
-	lastName: 'Song'
+
+AddressBook.Person.FIXTURES = [
+	{ id:1, firstName: 'Ariel', lastName: 'unicorn' },
+	{ id:2, firstName: 'Grant', lastName: 'Stampfli' },
+	{ id:3, firstName: 'Tian', lastName: 'Song' }
+];
+
+AddressBook.ApplicationRoute = Ember.Route.extend({
+	model: function () {
+		return this.store.find('person', 1);
+	}
 });
-console.log(ironman);
+
+
+
+
+
+// store.CreateRecord('post', {
+// 	firstName: 'Tian',
+// 	lastName: 'Song'
+// });
+
+// post.save();
+// store.find('person', 1);
